6771830eae0d6cf6d460c73b4b4a21752e2a78ca
ehaala <ejhaala@gmail.com>
2017-10-23 12:40:28 -0700

    component structure set up

---
+++ client/src/RadarChart.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Main from './Main';
+
+
+class RadarChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state {
+
+    }
+  }
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default RadarChart;
