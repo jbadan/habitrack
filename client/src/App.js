a36e952fb3d90fc27768e13009e77786d5c2cc44
foozie3moons <matthew.bell.290@gmail.com>
2017-10-23 12:16:44 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import './App.css';
+import Signup from './Signup';
+import Login from './Login';
+import AuthenticatedRoute from './AuthenticatedRoute';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      token: {}
+    }
+  }
+
+  liftTokenToState = (token) => {
+    this.setState({token: token})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="SignupBox">
+          <Signup lift={this.liftTokenToState} />
+        </div>
+        <div className="LoginBox">
+          <Login lift={this.liftTokenToState} />
+        </div>
+        <div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/AuthenticatedRoute.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class AuthenticatedRoute extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      token: localStorage.getItem('mernToken')
+    }
+  }
+
+  getOutput = () => {
+    if (this.state.token) {
+      return (<p>You are authorized to view this text</p>)
+    } else {
+      return (<p>INTRUDER ALERT! INTRUDER ALERT!</p>)
+    }
+  }
+
+  render() {
+    return this.getOutput();
+  }
+}
+
+export default AuthenticatedRoute;
