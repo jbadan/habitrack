23cb609cc4552b51719bdc7168bbfb2533d7483f
foozie3moons <matthew.bell.290@gmail.com>
2017-10-24 11:04:31 -0700

    gridlines added, but x scale gridlines are off for some reason

--- client/src/LineChart.js
+++ client/src/LineChart.js
@@ -7,32 +7,32 @@ class LineChart extends Component {
 
   render() {
     let data = [
-      {date:'1-May-2017',count:Math.floor(Math.random() * 11)},
-      {date:'30-Apr-2017',count:Math.floor(Math.random() * 11)},
-      {date:'27-Apr-2017',count:Math.floor(Math.random() * 11)},
-      {date:'26-Apr-2017',count:Math.floor(Math.random() * 11)},
-      {date:'25-Apr-2017',count:Math.floor(Math.random() * 11)},
-      {date:'24-Apr-2017',count:Math.floor(Math.random() * 11)},
-      {date:'23-Apr-2017',count:Math.floor(Math.random() * 11)},
+      {date:'21-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'20-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'19-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'18-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'17-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'16-Apr-2017',count:Math.floor(Math.random() * 11)},
+      {date:'15-Apr-2017',count:Math.floor(Math.random() * 11)},
+      {date:'14-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'13-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'12-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'11-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'10-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'9-Apr-2017',count:Math.floor(Math.random() * 11)},
+      {date:'8-Apr-2017',count:Math.floor(Math.random() * 11)},
+      {date:'7-Apr-2017',count:Math.floor(Math.random() * 11)},
+      {date:'6-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'5-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'4-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'3-Apr-2017',count:Math.floor(Math.random() * 11)},
       {date:'2-Apr-2017',count:Math.floor(Math.random() * 11)},
+      {date:'1-Apr-2017',count:Math.floor(Math.random() * 11)},
+      {date:'31-Mar-2017',count:Math.floor(Math.random() * 11)},
       {date:'30-Mar-2017',count:Math.floor(Math.random() * 11)},
       {date:'29-Mar-2017',count:Math.floor(Math.random() * 11)},
       {date:'28-Mar-2017',count:Math.floor(Math.random() * 11)},
-      {date:'27-Mar-2017',count:Math.floor(Math.random() * 11)},
-      {date:'26-Mar-2017',count:Math.floor(Math.random() * 11)}
+      {date:'27-Mar-2017',count:Math.floor(Math.random() * 11)}
     ]
     // set the dimensions and margins of the graph
     let margin = {top: 20, right: 20, bottom: 30, left: 50},
@@ -48,6 +48,10 @@ class LineChart extends Component {
       d.date = Date.parse(d.date);
       d.count = +d.count;
     });
+
+    let scale = d3.scaleLinear()
+      .domain( [0, 100] )
+      .range( [0, width] );
     // set the ranges
     let x = d3.scaleTime().range([0, width]);
     let y = d3.scaleLinear().range([height, 0]);
@@ -62,12 +66,6 @@ class LineChart extends Component {
     let tooltip = d3.select("body").append("div")
       .attr("class", "tooltip")
       .style("opacity", 0);
-
-    // Gridline
-    let gridlines = d3.axisBottom()
-      .tickFormat("")
-      .tickSize(-height)
-      .scale(y);
 
     // append the svg object to the body of the page
     // appends a 'group' element to 'svg'
@@ -85,6 +83,25 @@ class LineChart extends Component {
     // Get the data and load it to the domain
     x.domain(d3.extent(data, function(d) { return d.date; }));
     y.domain([0, d3.max(data, function(d) { return d.count; })]);
+
+    // Add gridlines
+    let xLines = d3.axisBottom(x)
+      .tickFormat("")
+      .tickSize(height)
+      .scale(scale);
+
+    let yLines = d3.axisRight(y)
+      .tickFormat("")
+      .tickSize(width)
+      .scale(y);
+
+    svg.append("g")
+      .attr("class", "grid")
+      .call(xLines);
+
+    svg.append("g")
+      .attr("class", "grid")
+      .call(yLines);
 
     // Add the valueline path.
     svg.append("g")
@@ -120,11 +137,7 @@ class LineChart extends Component {
     // Add the X Axis
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x)
-          .tickFormat(xTime)
-          .tickSize(-height)
-          .scale(x)
-        );
+        .call(d3.axisBottom(x).ticks(10).tickFormat(xTime));
 
     // Add the Y Axis
     svg.append("g")
