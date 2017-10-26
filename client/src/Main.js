9970c6e172806634b0054102ed74295867ecca82
laurenperez <laurenashleymarks@gmail.com>
2017-10-25 23:07:40 -0700

    added about bones

--- client/src/Main.js
+++ client/src/Main.js
@@ -14,7 +14,55 @@ class Main extends Component {
 		return (
 			<div>
 				<div>
-					<h3>Get Started with HabiTracker</h3>
+					<h3>Write some things about HabiTracker...</h3>
+
+					<div className="about-section">
+
+						<div className="dev-profile">
+							<h4>Jenna Badanowski</h4>
+							<img src="http://lorempixel.com/100/100/cats"/>
+							<p>Lorem ipsum dolor sit amet et delectus
+							 accommodare his consul copiosae legendos
+							  at vix ad putent delectus delicata usu.
+								Vidit dissentiet eos cu eum an brute
+								copiosae hendrerit. Eos erant
+								dolorum an. Per facer</p>
+						</div>
+
+						<div className="dev-profile">
+							<h4>Matthew Bell</h4>
+							<img src="http://lorempixel.com/100/100/cats"/>
+							<p>Lorem ipsum dolor sit amet et delectus
+							 accommodare his consul copiosae legendos
+							  at vix ad putent delectus delicata usu.
+								Vidit dissentiet eos cu eum an brute
+								copiosae hendrerit. Eos erant
+								dolorum an. Per facer</p>
+						</div>
+
+						<div className="dev-profile">
+						 <h4>Evan Haala</h4>
+						 <img src="http://lorempixel.com/100/100/cats"/>
+							<p>Lorem ipsum dolor sit amet et delectus
+							 accommodare his consul copiosae legendos
+							  at vix ad putent delectus delicata usu.
+								Vidit dissentiet eos cu eum an brute
+								copiosae hendrerit. Eos erant
+								dolorum an. Per facer</p>
+						</div>
+
+						<div className="dev-profile">
+							<h4>Lauren Perez</h4>
+							<img src="http://lorempixel.com/100/100/cats"/>
+							<p>Lorem ipsum dolor sit amet et delectus
+							 accommodare his consul copiosae legendos
+							  at vix ad putent delectus delicata usu.
+								Vidit dissentiet eos cu eum an brute
+								copiosae hendrerit. Eos erant
+								dolorum an. Per facer</p>
+						</div>
+
+					</div>
 				</div>
 			</div>
 		)
